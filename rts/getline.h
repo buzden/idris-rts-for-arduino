acfc4ed530014a6615a56ca14cd9521a035b14c0
Denis Buzdalov <public@buzden.ru>
2020-05-10 03:12:26 +0300

    RTS was made to be compilable with compiler for AVR architecture.

--- rts/getline.h
+++ rts/getline.h
@@ -2,6 +2,9 @@
 #define GETLINE_H
 #include <stdlib.h>
 #include <stdio.h>
+
+typedef long ssize_t; // True for AVR
+
 ssize_t getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp);
 ssize_t getline(char **buf, size_t *bufsiz, FILE *fp);
 #endif // GETLINE_H
